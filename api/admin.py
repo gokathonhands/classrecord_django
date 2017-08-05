23c3be24ea431efcfbcb39e6ff1469b02617c78a
pjo901018 <pjo901018@korea.ac.kr>
2017-08-05 18:33:21 +0900

    Admin page Course추가

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from api.models import Semester
+from api.models import Semester, Course
 
 admin.site.register(Semester)
+admin.site.register(Course)
