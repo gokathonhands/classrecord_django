5ca790e980bda1eb6cc2fa7a1d614e1a461031c3
pjo901018 <pjo901018@korea.ac.kr>
2017-08-05 17:43:09 +0900

    requirements.txt

--- api/models.py
+++ api/models.py
@@ -12,3 +12,16 @@ class Semester(models.Model):
 
     def __str__(self):
         return self.year + self.season
+
+class Course(models.Model):
+    # user = models.ForeignKey('auth.User', default=1)
+    semester_id = models.CharField()
+    year = models.IntegerField(default=2017)
+    season = models.CharField(max_length=100, default="spring")
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('year', 'season',)
+
+    def __str__(self):
+        return self.year + self.season
